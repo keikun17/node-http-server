d33fdc763a9982a38204cdb1de5b25dc79db30b0
keikun17 <keikun17@gmail.com>
2012-12-21 17:47:02 +0800

    pathname handling, testing blocking requests

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,9 +1,21 @@
+var exec =  require("child_process").exec;
+
 function start() {
-  console.log("Request handler 'start' was called.");
+  console.log("request handler 'start was called.'");
+
+  var content = "empty";
+
+  exec("pwd ", function(error, stdout, stderr) {
+    content = stdout; 
+    console.log("oyeah" + content);
+  });
+
+  return content;
 }
 
 function upload() {
   console.log("Request handler 'upload' was called.")
+  return "Hello Upload"
 }
 
 exports.start = start;
