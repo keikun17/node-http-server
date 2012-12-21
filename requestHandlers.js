cc0b296a4c73a2f548d8fadbf1c439500a586c94
keikun17 <keikun17@gmail.com>
2012-12-21 18:16:56 +0800

    bigger test request for 'start' to check if it is really blocking

--- requestHandlers.js
+++ requestHandlers.js
@@ -3,16 +3,25 @@ var exec =  require("child_process").exec;
 function start(response) {
   console.log("request handler 'start was called.'");
 
-  exec("ls -lah", function(error, stdout, stderr) {
-    response.writeHead(200, {"Content-Type": "text/plain"});
-    response.write(stdout);
-    response.end();
-  });
+  // exec("ls -lah", function(error, stdout, stderr) {
+  //   response.writeHead(200, {"Content-Type": "text/plain"});
+  //   response.write(stdout);
+  //   response.end();
+  // });
+  //
+
+  exec("find /",
+       { timeout: 1000, maxBuffer: 20000*1024 },
+       function(error, stdout, stderr){
+         response.writeHead(200, {"Content-Type": "text/plain"});
+         response.write(stdout);
+         response.end();
+       });
 }
 
 function upload(response) {
   console.log("Request handler 'upload' was called.");
-  response.writreHead(200, {"Content-Type": "text/plain"});
+  response.writeHead(200, {"Content-Type": "text/plain"});
   response.write("Uploadin Yo Files");
   response.end();
 }
